ca7418808ab016cca60e4c70f64e606a2cdabe0e
ruifeng wen <vvrfxyz@gmail.com>
2025-04-28 09:22:43 +0800

    feat:

--- example/src/main/java/xyz/vvrf/reactor/dag/example/DagUsageExample.java
+++ example/src/main/java/xyz/vvrf/reactor/dag/example/DagUsageExample.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import xyz.vvrf.reactor.dag.spring.SpringDagEngine;
 
 import java.util.UUID;
 
